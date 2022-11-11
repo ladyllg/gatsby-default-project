2aefbeec43a972b7d9ba3de2287acf73494a974b
Laura Lima <lauralimaufam@gmail.com>
2022-11-11 13:33:06 -0300

    footer updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,8 @@
 import * as React from 'react'
 import Navbar from './navbar'
+import * as  styles from '../styles/layout.module.css'
+import { AiOutlineGithub, AiOutlineInstagram, AiOutlineLinkedin } from 'react-icons/ai';
+import { IconContext } from "react-icons";
 
 const Layout = ({ pageTitle, children }) => {
 
@@ -9,8 +12,21 @@ const Layout = ({ pageTitle, children }) => {
         <Navbar/>
         <main>{children}</main>
         <footer>
-          <p>{new Date().getFullYear()}, Built with Gatsby.</p>
-          <p>Designed & developed by Laura Lima.</p>
+          <div className={styles.footerWrap}>
+            <div className={styles.footerContainer}>
+              <div>
+                <p>{new Date().getFullYear()}, Built with Gatsby.</p>
+                <p>Designed & developed by Laura Lima.</p>
+              </div>
+              <div>
+              <IconContext.Provider value={{ size:"2rem" }}>
+                <AiOutlineInstagram/>&nbsp;&nbsp;&nbsp;
+                <AiOutlineGithub/>&nbsp;&nbsp;&nbsp;
+                <AiOutlineLinkedin/>                
+              </IconContext.Provider>
+              </div>
+            </div>
+          </div>        
         </footer>
       </div>
     </>
